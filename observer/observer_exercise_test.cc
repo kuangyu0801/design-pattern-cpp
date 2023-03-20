83eb7631b1f436f7dd93b7e16e1c561b9cfb96ae
Kuangyu <kuangyu.lumiere.li@gmail.com>
2023-03-20 07:17:02 -0700

    observer: Add test case

--- observer/observer_exercise_test.cc
+++ observer/observer_exercise_test.cc
@@ -1,9 +1,18 @@
 #include "gtest/gtest.h"
 
+#include "public/game.h"
+#include "public/rat.h"
+
 // Demonstrate some basic assertions.
-TEST(HelloTest, BasicAssertions) {
-  // Expect two strings not to be equal.
-  EXPECT_STRNE("hello", "world");
-  // Expect equality.
-  EXPECT_EQ(7 * 6, 42);
+TEST(Observer, RatGame) {
+  Game game;
+  
+  auto rat0 = std::make_unique<Rat>(game);
+  EXPECT_EQ(rat0->attack, 1);
+  auto rat1 = std::make_unique<Rat>(game);
+  EXPECT_EQ(rat0->attack, 2);
+  EXPECT_EQ(rat1->attack, 2);
+
+  rat1.reset();
+  EXPECT_EQ(rat0->attack, 1);
 }
